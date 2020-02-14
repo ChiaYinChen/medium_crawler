d3de16a8a02551bf99c86abfcb2c2ce1a5141830
Carol Chen <awdrg1210@gmail.com>
2020-02-14 17:27:39 +0800

    colorize different log levels

--- medium_crawler/__init__.py
+++ medium_crawler/__init__.py
@@ -1,2 +1,36 @@
+import copy
+
+import scrapy.utils.log
+from colorlog import ColoredFormatter
+
 __version_info__ = (0, 0, 1)
 __version__ = '.'.join(str(_) for _ in __version_info__)
+
+
+# colorize different log levels
+color_formatter = ColoredFormatter(
+    (
+        '%(log_color)s%(levelname)-5s%(reset)s '
+        '%(yellow)s[%(asctime)s]%(reset)s'
+        '%(white)s %(name)s %(funcName)s %(bold_purple)s:%(lineno)d%(reset)s '
+        '%(log_color)s%(message)s%(reset)s'
+    ),
+    datefmt='%y-%m-%d %H:%M:%S',
+    log_colors={
+        'DEBUG': 'blue',
+        'INFO': 'bold_cyan',
+        'WARNING': 'red',
+        'ERROR': 'bg_bold_red',
+        'CRITICAL': 'red,bg_white',
+    }
+)
+_get_handler = copy.copy(scrapy.utils.log._get_handler)
+
+
+def _get_handler_custom(*args, **kwargs):
+    handler = _get_handler(*args, **kwargs)
+    handler.setFormatter(color_formatter)
+    return handler
+
+
+scrapy.utils.log._get_handler = _get_handler_custom
